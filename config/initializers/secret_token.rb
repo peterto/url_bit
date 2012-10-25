58413405878df641e70150448f045e637f137f73
Peter To <pt9386@gmail.com>
2012-10-24 21:39:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlBit::Application.config.secret_token = '6b21b8f97b4ad3a6cdaa81dd21f917f0edeb04d6212b06c7d8fdc418a2108e71270ca02de503d32e9d21f181af90e5e2fae2a0bc313e4bb18a9f16a36865a260'
