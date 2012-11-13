b58d221d4495a4e71c89fa2aead43c2777274d0e
Shaan Batra <sb1752@gmail.com>
2012-11-13 00:20:15 -0500

    adds tests to url controller spec and all tests pass

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -17,17 +17,27 @@ describe UrlsController do
 	end
 
 	describe "post create" do
-		it "should respond successfully" do
-			post :create
-			response.should be_successful
-		end
-	end
+		it "should create new url and redirect to the new url path" do
+      url = FactoryGirl.build(:url)
+      post :create, :url => {:source => url.source }
+      assigns[:url].persisted?.should be_true
+      flash[:notice].should == "URL was created successfully"
+      response.should redirect_to url_path(assigns[:url])
+    end
+
+    it "should not create new url and redirect to existing url path" do
+      url = FactoryGirl.create(:url)
+      post :create, :url => {:source => url.source }
+      assigns[:url].should be_nil
+      flash[:notice].should == "URL already exists"
+      response.should redirect_to url_path(url)
+    end
+  end
 
 	describe "get show" do
 		it "should respond successfully" do
-
 			url = FactoryGirl.create(:url)
-			get :show, :id => url.id 
+			get :show, :id => url.id
 			response.should be_successful
 		end
 	end
